7b5cc7e7bb78f1953a41da8c9e28ab9586c71a8e
melonaster3 <melonaster@gmail.com>
2023-10-17 17:41:49 +0900

    Adding Server for API calls

---
+++ client/src/components/index.jsx
@@ -0,0 +1,78 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+} from "chart.js";
+import { Bar, Line } from "react-chartjs-2";
+import React, { useEffect, useRef, useState } from "react";
+import LineGraph from "./Line";
+import { BarGraphType, GraphTypes, LineGraphTypes } from "../services/dummy";
+import { BarGraph } from "./Bar";
+export default function Graph() {
+  const [graphType, setGraphType] = useState("");
+  const [barType, setBarType] = useState("");
+  const [lineType, setLineType] = useState("");
+
+  return (
+    <>
+      <select
+        name="GraphType"
+        onChange={(e) => {
+          setGraphType(e.target.value);
+        }}
+      >
+        <option selected="selected"></option>
+        {GraphTypes.map(({ value, label }, index) => (
+          <option key={index} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+      <br />
+
+      {graphType === "Line" && (
+        <>
+          <select
+            name="LineType"
+            onChange={(e) => {
+              setLineType(e.target.value);
+            }}
+          >
+            <option selected="selected"></option>
+            {LineGraphTypes.map(({ value, label }, index) => (
+              <option key={index} value={value}>
+                {label}
+              </option>
+            ))}
+          </select>
+          {lineType && <LineGraph type={lineType} />}
+        </>
+      )}
+
+      {graphType === "Bar" && (
+        <>
+          <select
+            name="BarType"
+            onChange={(e) => {
+              setBarType(e.target.value);
+            }}
+          >
+            <option selected="selected"></option>
+            {BarGraphType.map(({ value, label }, index) => (
+              <option key={index} value={value}>
+                {label}
+              </option>
+            ))}
+          </select>
+          {barType && <BarGraph type={barType} />}
+        </>
+      )}
+    </>
+  );
+}
